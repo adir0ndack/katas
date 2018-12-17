82303d794ea490966e27782f1478c5da36db296f
shikaridsg <45698399+shikaridsg@users.noreply.github.com>
2018-12-17 00:30:59 -0500

    Update [5kyu] "Moving Zeros to the End".py

--- [5kyu] "Moving Zeros to the End".py
+++ [5kyu] "Moving Zeros to the End".py
@@ -7,3 +7,5 @@ def move_zeros(array):
 # Here, sorted function is sorting by lambda function, which turns non-zeros and booleans into false and 0 into true, and
 # then sort by keys where all false comes before the trues... Python uses stable sort so all values in false and true remain 
 # in their original order in output.
+
+# This is a stupid and unreadable way to do it but it looks nice so style > function as they say
